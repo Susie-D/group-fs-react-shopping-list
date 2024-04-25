6cea3da5763a2224b91e0d2c847a378531b92ad6
Steven Karl <Karl.Steven09@gmail.com>
2024-04-25 17:18:32 -0500

    We have our item POST route working

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,45 +1,85 @@
-import React from 'react';
-import Header from '../Header/Header.jsx';
-import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-
+import React from "react";
+import Header from "../Header/Header.jsx";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-const [shoppingList, setShoppingList] = useState([]);
-// useEffect runs after page renders, then calls function/ReactHook
-// shoppingList houses default values, in order to update the state we need setShoppingList (useState functionality)
-useEffect(() => {
+  const [shoppingList, setShoppingList] = useState([]);
+  let [shoppingFormItem, setShoppingFormItem] = useState("Mahad");
+  // let[shoppingFormQuantity, setShoppingFormQuantity] = useState("");
+  // let[shoppingFormUnit, setShoppingFormUnit] = useState("");
+  // let[shoppingFormBuy, setShoppingFormBuy] = useState("");
+
+  // useEffect runs after page renders, then calls function/ReactHook
+  // shoppingList houses default values, in order to update the state we need setShoppingList (useState functionality)
+  useEffect(() => {
     fetchShoppingList();
-}, [])
-const fetchShoppingList = () => {
+  }, []);
+  const fetchShoppingList = () => {
     axios({
-        method: 'GET',
-        url: '/api/shoppingList'
+      method: "GET",
+      url: "/api/shoppingList",
     })
-    .then((response) => {
+      .then((response) => {
         console.log("WE HAVE THE LIST:", response.data);
         setShoppingList(response.data);
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         console.log("We have an error", error);
-    })
-};
-    return (
-        <div className="App">
-            <Header />
-            <main>
-            <ul>
-                {shoppingList.map((item) => {
-                    return (
-                    <li key={item.id}> {item.item} {item.quantity} {item.unit}</li> )
-                })}
-                
-            </ul>
-            </main>
-        </div>
-    );
+      });
+  };
+
+  //! POST FUNCTION
+
+  const addItem = () => {
+    axios
+      .post("/api/shoppingList", { item: shoppingFormItem })
+      .then((response) => {
+        setShoppingFormItem("");
+        fetchShoppingList();
+      })
+      .catch((err) => {
+        alert("Error Adding Item");
+        console.log(err);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //! We need to add more input variables below
+    if (shoppingFormItem) {
+      addItem();
+    } else {
+      alert("We Need an Item");
+    }
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Item"
+            value={shoppingFormItem}
+            onChange={(evt) => setShoppingFormItem(evt.target.value)}
+          ></input>
+          <button type="submit">Add Item</button>
+        </form>
+        <ul>
+          {shoppingList.map((item) => {
+            return (
+              <li key={item.id}>
+                {item.item} {item.quantity} {item.unit}
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default App;
